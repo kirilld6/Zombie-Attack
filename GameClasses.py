0ead66883760f91e869fb8b65533fcd88d2fd397
Media <media@mal.ru>
2023-02-09 10:36:02 +0300

    Add Zombie, Shooting and collisions

--- GameClasses.py
+++ GameClasses.py
@@ -1,12 +1,14 @@
 # Создаем класс игрока на основе pygame.sprite.Sprite
-import pygame
+import pygame as pg
+from random import randrange
 from Constants import *
 
 
-class Player(pygame.sprite.Sprite):
+# Класс игрока на основе класса Sprite библиотеки pygame
+class Player(pg.sprite.Sprite):
     def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((50, 40))
+        pg.sprite.Sprite.__init__(self)
+        self.image = pg.Surface((50, 40))
         self.image.fill(GREEN)
         self.rect = self.image.get_rect()
         self.rect.centerx = SCREEN_WIDTH / 2
@@ -17,13 +19,13 @@ class Player(pygame.sprite.Sprite):
     def update(self):
         self.speed_x = 0
         self.speed_y = 0
-        if pygame.key.get_pressed()[pygame.K_LEFT]:
+        if pg.key.get_pressed()[pg.K_LEFT] or pg.key.get_pressed()[pg.K_a]:
             self.speed_x = -8
-        if pygame.key.get_pressed()[pygame.K_RIGHT]:
+        if pg.key.get_pressed()[pg.K_RIGHT] or pg.key.get_pressed()[pg.K_d]:
             self.speed_x = 8
-        if pygame.key.get_pressed()[pygame.K_UP]:
+        if pg.key.get_pressed()[pg.K_UP] or pg.key.get_pressed()[pg.K_w]:
             self.speed_y = -8
-        if pygame.key.get_pressed()[pygame.K_DOWN]:
+        if pg.key.get_pressed()[pg.K_DOWN] or pg.key.get_pressed()[pg.K_s]:
             self.speed_y = 8
         self.rect.x += self.speed_x
         self.rect.y += self.speed_y
@@ -31,3 +33,44 @@ class Player(pygame.sprite.Sprite):
             self.rect.right = SCREEN_HEIGHT
         if self.rect.left < 0:
             self.rect.left = 0
+
+    def player_shooter(self):
+        return Bullet(self.rect.centerx, self.rect.top)
+
+
+# Класс врага, в нашем случае зомби на основе класса Sprite библиотеки pygame
+class Zombie(pg.sprite.Sprite):
+    def __init__(self):
+        pg.sprite.Sprite.__init__(self)
+        self.image = pg.Surface((30, 40))
+        self.image.fill(RED)
+        self.rect = self.image.get_rect()
+        self.rect.x = randrange(SCREEN_WIDTH - self.rect.width)
+        self.rect.y = randrange(-120, -60)
+        self.speedy = randrange(1, 3)
+
+    def update(self):
+        self.rect.y += self.speedy
+        # При достижении нижней части экрана перемещвем зомби обратно на верх в новую координату x,y
+        # и задаем новое значение скорости движения
+        if self.rect.top > SCREEN_HEIGHT + 10:
+            self.rect.x = randrange(SCREEN_WIDTH - self.rect.width)
+            self.rect.y = randrange(-100, -40)
+            self.speedy = randrange(1, 3)
+
+
+class Bullet(pg.sprite.Sprite):
+    def __init__(self, x, y):
+        pg.sprite.Sprite.__init__(self)
+        self.image = pg.Surface((10, 10))
+        self.image.fill(YELLOW)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = x
+        self.rect.bottom = y
+        self.speed_y = -10
+
+    def update(self):
+        self.rect.y += self.speed_y
+        # Удаляем пулю если она улетела за пределы экрана
+        if self.rect.bottom < 0:
+            self.kill()
--- main.py
+++ main.py
@@ -1,19 +1,28 @@
-import pygame
+from random import randrange
+
+import pygame as pg
 from Constants import *
-from GameClasses import Player
+
+from GameClasses import Player, Zombie, Bullet
 
 
 def main():
     # Инициализация PyGame и создание игрового окна
-    pygame.init()
-    pygame.mixer.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    pygame.display.set_caption("Zombie Attack")
-    clock = pygame.time.Clock()
+    pg.init()
+    pg.mixer.init()
+    screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pg.display.set_caption("Zombie Attack")
+    clock = pg.time.Clock()
 
-    all_sprites = pygame.sprite.Group()
+    all_sprites = pg.sprite.Group()
+    zombies = pg.sprite.Group()
+    bullets = pg.sprite.Group()
     player = Player()
     all_sprites.add(player)
+    for _ in range(randrange(3, 9)):
+        zombie = Zombie()
+        all_sprites.add(zombie)
+        zombies.add(zombie)
 
     # Основной игровой цикл
     running = True
@@ -21,21 +30,38 @@ def main():
         # keep loop running at the right speed
         clock.tick(FPS)
         # Process input (events)
-        for event in pygame.event.get():
+        for event in pg.event.get():
             # check for closing window
-            if event.type == pygame.QUIT:
+            if event.type == pg.QUIT:
                 running = False
+            elif event.type == pg.KEYDOWN:
+                if event.key == pg.K_SPACE:
+                    bullet = player.player_shooter()
+                    all_sprites.add(bullet)
+                    bullets.add(bullet)
 
-        # Update
+        # Обновляем группу со спрайтами спрайты
         all_sprites.update()
+
+        # проверяем на столкновения Игрока и зомби
+        collisions = pg.sprite.spritecollide(player, zombies, False)
+        if collisions:
+            running = False
+
+        # проверяем на столкновения сняряда и зомби
+        collisions = pg.sprite.groupcollide(zombies, bullets, True, True)
+        for _ in collisions:
+            zombie = Zombie()
+            all_sprites.add(zombie)
+            zombies.add(zombie)
 
         # Отрисовка объектов на экране
         screen.fill(BLACK)
         all_sprites.draw(screen)
         # *after* drawing everything, flip the display
-        pygame.display.flip()
+        pg.display.flip()
 
-    pygame.quit()
+    pg.quit()
 
 
 if __name__ == '__main__':
